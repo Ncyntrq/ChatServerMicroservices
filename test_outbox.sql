9d91e98f127a29acb2c9a7431fff381e46dc435d
Thanh Nguyen <tothanhnguyen2006@gmail.com>
2026-06-25 22:32:23 +0700

    Delete test_outbox.sql

--- test_outbox.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO outbox_messages (aggregate_type, aggregate_id, exchange, routing_key, payload, status, created_at) 
-VALUES ('TEST', '1', 'chat.fanout', '', '{"test": "hello"}', 'PENDING', NOW());
